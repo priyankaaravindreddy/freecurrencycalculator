85dcb3f0578d70a7acc22043220304a13f36596c
Mahajan <priyanka.b.mahajan@accenture.com>
2024-04-07 09:05:34 +0530

    chore(cssmodule): added css modules

--- src/components/CurrencyTable.jsx
+++ src/components/CurrencyTable.jsx
@@ -1,29 +1,38 @@
-import React from 'react';
-import './CurrencyTable.css';
+import React from "react";
+import styles from"./CurrencyTable.module.css";
 
-const CurrencyTable = ({ selectedCurrencies, lastSevenDays, exchangeRates, handleRemoveCurrency }) => {
+const CurrencyTable = ({
+  selectedCurrencies,
+  lastSevenDays,
+  exchangeRates,
+  handleRemoveCurrency,
+}) => {
   return (
-    <div className="currency-table-container">
-      <div className="currency-table">
-        <div className="header">
+    <div className={styles["currency-table-container"]}>
+      <div className={styles["currency-table"]}>
+        <div className={styles.header}>
           <div>Currency</div>
           <div>Exchange Rate</div>
           <div>Action</div>
         </div>
-        <div className="body">
+        <div className={styles.body}>
           {selectedCurrencies.map((currency, index) => (
-            <div key={currency} className="row">
-              <div className="currency">{currency}</div>
-              <div className="exchange-rate-list">
+            <div key={currency} className={styles.row}>
+              <div className={styles.currency}>{currency}</div>
+              <div className={styles["exchange-rate-list"]}>
                 {lastSevenDays.map((date) => (
-                  <div key={date} className="exchange-rate">
-                    <span className="date">{date}</span>
-                    <span className="rate">{exchangeRates[date]?.[currency]}</span>
+                  <div key={date} className={styles["exchange-rate"]}>
+                    <span className={styles.date}>{date}</span>
+                    <span className={styles.rate}>
+                      {exchangeRates[date]?.[currency]}
+                    </span>
                   </div>
                 ))}
               </div>
               <button
-                className="remove-button"
+                className={`${styles["remove-button"]} ${
+                  selectedCurrencies.length === 3 ? styles["disabled"] : ""
+                }`}
                 disabled={selectedCurrencies.length === 3}
                 onClick={() => handleRemoveCurrency(index)}
               >
